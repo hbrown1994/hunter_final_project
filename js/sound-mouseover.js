f2835db208de54801f8bb7e16988957c12b25d33
Hunter Brown <hunterbrownmusic1@gmail.com>
2020-03-07 13:42:37 -0600

    complete_FB_module_FINAL

---
+++ js/sound-mouseover.js
@@ -0,0 +1,121 @@
+// All Code in this Section was borrowed from: http://www.allwebco-templates.com/support/S_audio_onmouseover.html
+
+
+////////////////////////////////////////////////////////////////////////////////////
+function playclip_facebook_1() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "click.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("sound_1"); audio.play();
+audio.play();
+}
+}
+}
+////////////////////////////////////////////////////////////////////////////////////
+
+function playclip_facebook_2() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "click.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("sound_2"); audio.play();
+audio.play();
+}
+}
+}
+////////////////////////////////////////////////////////////////////////////////////
+
+function playclip_facebook_3() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "click.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("sound_3"); audio.play();
+audio.play();
+}
+}
+}
+////////////////////////////////////////////////////////////////////////////////////
+
+function playclip_facebook_4() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "click.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("sound_4"); audio.play();
+audio.play();
+}
+}
+}
+////////////////////////////////////////////////////////////////////////////////////
+
+function playclip_facebook_5() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "click.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("sound_5"); audio.play();
+audio.play();
+}
+}
+}
+////////////////////////////////////////////////////////////////////////////////////
+
+function playclip_facebook_6() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "click.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("sound_6"); audio.play();
+audio.play();
+}
+}
+}
+////////////////////////////////////////////////////////////////////////////////////
+
+function playclip_facebook_7() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "click.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("sound_7"); audio.play();
+audio.play();
+}
+}
+}
