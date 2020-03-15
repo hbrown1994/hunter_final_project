d87a70b366acbb5775b12f0935fb09e1515588a9
Hunter Brown <hunterbrownmusic1@gmail.com>
2020-03-15 16:49:35 -0500

    FINAL_commit

--- js/sound-mouseover.js
+++ js/sound-mouseover.js
@@ -1,7 +1,8 @@
 // All Code in this Section was borrowed from: http://www.allwebco-templates.com/support/S_audio_onmouseover.html
+//These modules/functions enable sound files to be triggered on mouse hover
 
 
-////////////////////////////////////////////////////////////////////////////////////
+//-----------------Facebook----------------//////////////////////////////////////////////////////////////////////////////////
 function playclip_facebook_1() {
 if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
 if (document.all)
@@ -28,7 +29,6 @@ audio.pause();
 }
 }
 ////////////////////////////////////////////////////////////////////////////////////
-
 function playclip_facebook_2() {
 if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
 if (document.all)
@@ -55,7 +55,6 @@ audio.pause();
 }
 }
 ////////////////////////////////////////////////////////////////////////////////////
-
 function playclip_facebook_3() {
 if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
 if (document.all)
@@ -82,7 +81,6 @@ audio.pause();
 }
 }
 ////////////////////////////////////////////////////////////////////////////////////
-
 function playclip_facebook_4() {
 if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
 if (document.all)
@@ -109,7 +107,6 @@ audio.pause();
 }
 }
 ////////////////////////////////////////////////////////////////////////////////////
-
 function playclip_facebook_5() {
 if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
 if (document.all)
@@ -136,7 +133,6 @@ audio.pause();
 }
 }
 ////////////////////////////////////////////////////////////////////////////////////
-
 function playclip_facebook_6() {
 if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
 if (document.all)
@@ -163,7 +159,6 @@ audio.pause();
 }
 }
 ////////////////////////////////////////////////////////////////////////////////////
-
 function playclip_facebook_7() {
 if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
 if (document.all)
@@ -180,8 +175,7 @@ audio.play();
 }
 }
 
-////////////////////////////////////////////////////////////////////////////////////
-
+//-----------------Google----------------//////////////////////////////////////////////////////////////////////////////////
 function playclip_google_9() {
 if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
 if (document.all)
@@ -194,13 +188,12 @@ else {
 {
 var audio = document.getElementById("google_sound_9"); audio.play();
 audio.play();
+var audio = document.getElementById("twitter_sound_7"); audio.pause();
+audio.pause();
 }
 }
 }
-
-
-////////////////////////////////////////////////////////////////////////////////////
-
+//-----------------Youtube----------------//////////////////////////////////////////////////////////////////////////////////
 function playclip_youtube_1() {
 if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
 if (document.all)
@@ -226,9 +219,7 @@ audio.pause();
 }
 }
 }
-
 ////////////////////////////////////////////////////////////////////////////////////
-
 function playclip_youtube_2() {
 if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
 if (document.all)
@@ -254,9 +245,7 @@ audio.pause();
 }
 }
 }
-
 ////////////////////////////////////////////////////////////////////////////////////
-
 function playclip_youtube_3() {
 if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
 if (document.all)
@@ -282,9 +271,7 @@ audio.pause();
 }
 }
 }
-
 ////////////////////////////////////////////////////////////////////////////////////
-
 function playclip_youtube_4() {
 if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
 if (document.all)
@@ -310,9 +297,7 @@ audio.pause();
 }
 }
 }
-
 ////////////////////////////////////////////////////////////////////////////////////
-
 function playclip_youtube_5() {
 if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
 if (document.all)
@@ -338,9 +323,7 @@ audio.pause();
 }
 }
 }
-
 ////////////////////////////////////////////////////////////////////////////////////
-
 function playclip_youtube_6() {
 if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
 if (document.all)
@@ -362,6 +345,70 @@ audio.pause();
 var audio = document.getElementById("youtube_sound_4"); audio.pause();
 audio.pause();
 var audio = document.getElementById("youtube_sound_6"); audio.pause();
+audio.pause();
+}
+}
+}
+//-----------------Twitter----------------//////////////////////////////////////////////////////////////////////////////////
+function playclip_twitter_7() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "click.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("twitter_sound_7"); audio.play();
+audio.play();
+var audio = document.getElementById("google_sound_9"); audio.pause();
+audio.pause();
+}
+}
+}
+//-----------------Instagram----------------//////////////////////////////////////////////////////////////////////////////////
+function playclip_instagram_0() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "click.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("instagram_sound_0"); audio.play();
+audio.play();
+var audio = document.getElementById("twitter_sound_7"); audio.pause();
+audio.pause();
+var audio = document.getElementById("google_sound_9"); audio.pause();
+audio.pause();
+var audio = document.getElementById("youtube_sound_1"); audio.pause();
+audio.pause();
+var audio = document.getElementById("youtube_sound_2"); audio.pause();
+audio.pause();
+var audio = document.getElementById("youtube_sound_3"); audio.pause();
+audio.pause();
+var audio = document.getElementById("youtube_sound_4"); audio.pause();
+audio.pause();
+var audio = document.getElementById("youtube_sound_5"); audio.pause();
+audio.pause();
+var audio = document.getElementById("youtube_sound_6"); audio.pause();
+audio.pause();
+var audio = document.getElementById("facebook_sound_1"); audio.pause();
+audio.pause();
+var audio = document.getElementById("facebook_sound_2"); audio.pause();
+audio.pause();
+var audio = document.getElementById("facebook_sound_3"); audio.pause();
+audio.pause();
+var audio = document.getElementById("facebook_sound_4"); audio.pause();
+audio.pause();
+var audio = document.getElementById("facebook_sound_5"); audio.pause();
+audio.pause();
+var audio = document.getElementById("facebook_sound_6"); audio.pause();
+audio.pause();
+var audio = document.getElementById("facebook_sound_7"); audio.pause();
 audio.pause();
 }
 }
